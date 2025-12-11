75b22f35ae2b702efdf3a37fc90c837927b41ac9
Artem <rapid9791@gmail.com>
2025-12-11 22:57:36 +0300

    refactor: switch from SQLite to PostgreSQL

--- app/db/db_config.py
+++ app/db/db_config.py
@@ -1,10 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 import typing
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
 
-engine = create_async_engine("sqlite+aiosqlite:///db.sqlite3")
+engine = create_async_engine(DB_URL, future=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
