8b37b86c9495f38cf2fcd0487094c31947fab1f2
Artem <rapid9791@gmail.com>
2025-12-12 18:29:26 +0300

    fix: change names in testing , make the same db settings in settings.toml as in default.toml, add env_switcher and env_var_prefix, delete .env file

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,14 @@
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
+    env_var_prefix='PAYMENT_SERVICE',
     settings_files=[
         'app/config/default.toml',
         'app/config/settings.toml',
         'app/config/secrets.toml',
         'app/config/testing.toml',
     ],
+    env_switcher="PAYMENT_SERVICE_ENV",
     environments=True,
     load_dotenv=False,
     uppercase_keys=True
