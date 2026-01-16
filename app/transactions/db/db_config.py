7849d31e21990bf7a517dcf293baef508c8d9c66
Artem <rapid9791@gmail.com>
2026-01-16 19:26:56 +0300

    feat: add pgbouncer and refactor configuration
    1)implement pgbouncer
    2)move environment variables to config file
    3)update token expiration time

--- app/transactions/db/db_config.py
+++ app/transactions/db/db_config.py
@@ -6,17 +6,17 @@ from app.transactions.settings import settings
 logger = logging.getLogger(__name__)
 
 db_url = settings.get("DB.URL")
-logger.info(f"DataBaseConnection Url: {db_url}")
+logger.info(f"Database URL: {db_url}")
 
 db = Database(
     database_url=db_url,
-    echo=settings.get("DB.ECHO", settings.get("db.echo", False)),
-    pool_size=settings.get("DB.POOL_SIZE", settings.get("db.pool_size", 20)),
-    max_overflow=settings.get("DB.MAX_OVERFLOW", settings.get("db.max_overflow", 10)),
+    echo=settings.get("DB.ECHO", False),
+    pool_size=settings.get("DB.POOL_SIZE"),
+    max_overflow=settings.get("DB.MAX_OVERFLOW"),
+    pool_recycle=settings.get("DB.POOL_RECYCLE"),
 )
 
 session_maker = db.session_maker
-
 
 get_async_session = db.get_async_session
 create_db_and_tables = db.create_db_and_tables
