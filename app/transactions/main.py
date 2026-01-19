db12bc4bb7efddc73934ad09676bcfe8bbe19cac
Artem <rapid9791@gmail.com>
2026-01-19 23:21:16 +0300

    feat: add kafka event-driven communication

---
+++ app/transactions/main.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from app.outbox.balance_event_handler import BalanceEventHandler
+from app.outbox.kafka_consumer import init_kafka_consumer
+from app.outbox.kafka_producer import kafka_producer
+from app.outbox.outbox_processor import OutboxProcessor
+from app.transactions.api.v1 import router as transactions_router_v1
+from app.transactions.db.db_config import create_db_and_tables, db
+from app.transactions.events.transaction_event_handler import TransactionEventHandler
+from app.transactions.events.user_event_handlers import (
+    handle_user_blocked,
+    handle_user_created,
+)
+from app.transactions.taskiq_broker import broker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+
+    if not broker.is_worker_process:
+        logger.info("Starting taskiq broker...")
+        await broker.startup()
+        logger.info("Broker taskiq started successfully")
+
+    handlers = [
+        TransactionEventHandler(),
+        BalanceEventHandler(),
+    ]
+    processor = OutboxProcessor(db.session_maker, handlers)
+    outbox_processor_task = asyncio.create_task(processor.start())
+    logger.info("Outbox transaction service processor started")
+
+    kafka_consumer = init_kafka_consumer(
+        bootstrap_servers="kafka:9092",
+        group_id="transaction-service",
+        topics=["user.created", "user.blocked", "user.updated"],
+    )
+
+    kafka_consumer.register_handler("user.created", handle_user_created)
+    kafka_consumer.register_handler("user.blocked", handle_user_blocked)
+
+    kafka_consumer_task = asyncio.create_task(kafka_consumer.start())
+    logger.info("Kafka Consumer started for User Service events")
+
+    await kafka_producer.start()
+
+    state = {
+        "database_ready": True,
+        "broker_ready": True,
+        "outbox_processor_ready": True,
+        "kafka_consumer_ready": True,
+    }
+    yield state
+
+    if outbox_processor_task:
+        await processor.stop()
+        await outbox_processor_task
+
+    if kafka_consumer_task:
+        await kafka_consumer.stop()
+        await kafka_consumer_task
+
+    await kafka_producer.stop()
+
+    if not broker.is_worker_process:
+        logger.info("Shutting down transaction broker...")
+        await broker.shutdown()
+        logger.info("Broker shutdown for transactions complete")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(transactions_router_v1)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
---
+++ app/users/main.py
@@ -0,0 +1,62 @@
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from app.outbox.balance_event_handler import BalanceEventHandler
+from app.outbox.kafka_consumer import init_kafka_consumer
+from app.outbox.kafka_producer import kafka_producer
+from app.outbox.outbox_processor import OutboxProcessor
+from app.users.api.v1 import router as v1_router
+from app.users.db.db_config import create_db_and_tables, db
+from app.users.events.update_balance import handle_balance_updated
+from app.users.events.user_event_handler import UserEventHandler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+
+    await kafka_producer.start()
+    logger.info("Kafka Producer started")
+
+    handlers = [UserEventHandler(), BalanceEventHandler()]
+    processor = OutboxProcessor(db.session_maker, handlers)
+    outbox_processor_task = asyncio.create_task(processor.start())
+    logger.info("Outbox user service processor started")
+
+    kafka_consumer = init_kafka_consumer(
+        bootstrap_servers="kafka:9092", group_id="user-service", topics=["balance.updated"]
+    )
+    kafka_consumer.register_handler("BALANCE_UPDATED", handle_balance_updated)
+
+    kafka_consumer_task = asyncio.create_task(kafka_consumer.start())
+    logger.info("Kafka Consumer started for Transaction Service events")
+
+    state = {
+        "database_ready": True,
+        "outbox_processor_ready": True,
+        "kafka_producer_ready": True,
+    }
+    yield state
+
+    if outbox_processor_task:
+        await processor.stop()
+
+    if kafka_consumer_task:
+        await kafka_consumer.stop()
+
+    await kafka_producer.stop()
+    logger.info("Kafka Producer stopped")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(v1_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
